b4b322f0e8cf7d0b34ace8ffc476f98d6c46e3c0
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-03-14 19:21:18 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,27 @@
+export const item = {
+    "id": "297545fb-9799-4d6a-b710-606a93372dc8",
+    "gallery": [
+        {
+            "id": "79d90ae3-9f69-493a-80d3-5b307a0912fe",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/193.png"
+        },
+        {
+            "id": "ab227510-bc62-4f62-ab9d-26c9c444e197",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/182.png"
+        },
+        {
+            "id": "28753c77-2afa-46af-a562-7910c9bbbc52",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/345.png"
+        },
+        {
+            "id": "f6a3c8a8-a839-4fa5-9cdc-9b9330ad5665",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/145.png"
+        },
+        {
+            "id": "9fd82590-8484-4cbe-b3d0-21dc581227da",
+            "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/62.png"
+        }
+    ],
+    "event_name": "capital",
+    "event_description": "Crime mouth admit receive should."
+}
